8767878ffd2aa55d5c947dbb73f7550897640862
Palledar Praveen <praveenkumar.p347@gmail.com>
2024-06-08 22:05:20 +0530

    Adding InMemory SpringSecurityAuthentication

--- src/main/java/com/springsecurity/Controller/DemoController.java
+++ src/main/java/com/springsecurity/Controller/DemoController.java
@@ -12,5 +12,19 @@ public class DemoController {
 	public String getDemo() {
 		return "<h1>Spring Security Demo Controller</h1>";
 	}
+	
+	@GetMapping("/one")
+	public String getDemoOne() {
+		return "<h1>Spring Security Demo One Controller</h1>";
+	}
+	
+	@GetMapping("/two")
+	public String getDemoTwo() {
+		return "<h1>Spring Security Demo Two Controller</h1>";
+	}
+	@GetMapping("/three")
+	public String getDemoThree() {
+		return "<h1>Spring Security Demo Three Controller</h1>";
+	}
 
 }
